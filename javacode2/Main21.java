4e6cedf75263e382c6d7a0923545a06c86a7cfa2
VincentTuan <vincentjava@yahoo.com.tw>
2025-03-11 19:39:51 +0800

    Add files via upload

---
+++ javacode2/Main21.java
@@ -0,0 +1,12 @@
+public class Main21 {
+	public static void main(String[] args) {
+		// 利用 if-else 來判斷成績是否及格
+		int score = 45;
+		System.out.printf("分數: %d%n", score);
+		if(score >= 60) {
+			System.out.println("及格");
+		} else {
+			System.out.println("不及格");
+		}
+	}
+}
