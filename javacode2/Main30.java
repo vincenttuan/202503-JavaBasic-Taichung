f9310fc50ea9382d4c8efefe2fcf619a77b6eac3
VincentTuan <vincentjava@yahoo.com.tw>
2025-03-13 19:47:06 +0800

    Add files via upload

--- javacode2/Main30.java
+++ javacode2/Main30.java
@@ -3,18 +3,17 @@ public class Main30 {
 
 	public static void main(String[] args) {
 		Random r = new Random();
-
+		int count = 0; // 計數器
 		do {
+			count++; // 計數器 + 1
 			// 隨機產生 0~99 的數字
 			// 若數字 = 0 則透過 break; 強制跳離迴圈
 			int num = r.nextInt(100);
-			System.out.println(num);
+			System.out.printf("%03d -> %02d%n", count, num);
 			if(num == 0) {
 				break;
 			}
 		} while(true);
-
-
 	}
 
 }
